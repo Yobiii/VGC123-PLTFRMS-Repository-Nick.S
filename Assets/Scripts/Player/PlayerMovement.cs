b4ecfbb9d8adde027fabec199d74fb30ad3f6f3c
Yobiii <nickshapira@hotmail.com>
2021-02-12 03:32:17 -0500

    Lab 4 - Broken

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Animator))]
-
+[RequireComponent(typeof(SpriteRenderer))]
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     Animator anim;
+    SpriteRenderer marioSprite;
 
     public float speed;
     public int jumpForce;
     public bool isGrounded;
-    public bool buttonDown;
     public LayerMask isGroundLayer;
     public Transform groundCheck;
     public float groundCheckRadius;
@@ -21,8 +21,10 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        marioSprite = GetComponent<SpriteRenderer>();
 
         if (speed <= 0)
         {
@@ -34,21 +36,22 @@ public class PlayerMovement : MonoBehaviour
             jumpForce = 100;
         }
 
-        if (groundCheckRadius <= 0) 
+        if (groundCheckRadius <= 0)
         {
             groundCheckRadius = 0.01f;
         }
 
         if (!groundCheck)
         {
-            Debug.Log("Groundcheck does not exist, please set a transform value for groundcheck.");
+            Debug.Log("Groundcheck does not exist, please set a transform value for groundcheck");
         }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
 
         if (Input.GetButtonDown("Jump") && isGrounded)
@@ -57,21 +60,11 @@ public class PlayerMovement : MonoBehaviour
             rb.AddForce(Vector2.up * jumpForce);
         }
 
-        
-        if (Input.GetButtonDown("Fire1"))
-        {
-                anim.SetTrigger("ctrlbuttonDown");
-        }
-
-        if (Input.GetButtonDown("Vertical"))
-        {
-            anim.SetTrigger("wbuttonDown");
-        }
-
-
         rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
         anim.SetFloat("speed", Mathf.Abs(horizontalInput));
         anim.SetBool("isGrounded", isGrounded);
 
-    }   
+        if (marioSprite.flipX && horizontalInput > 0 || !marioSprite.flipX && horizontalInput < 0)
+            marioSprite.flipX = !marioSprite.flipX;
+    }
 }
